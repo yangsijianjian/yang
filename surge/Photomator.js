a2ea5f98df625d6b360779f9c720097d0d045564
yangsijian <yangsijian2022@gmail.com>
2025-02-13 06:34:57 +0800

    更新 Photomator.js

--- surge/Photomator.js
+++ surge/Photomator.js
@@ -6,8 +6,8 @@
 使用声明：️仅供学习交流, 🈲️商业用途
 ********************************
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/photomator.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/photomator.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/yangsijianjian/yang/refs/heads/main/surge/Photomator.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/yangsijianjian/yang/refs/heads/main/surge/Photomator.js
 [mitm] 
 hostname = api.revenuecat.com
 *******************************/
